8d6e51df826e37ba3298ab8229788eb45e2e9f59
Tioma BOSENKO <tiomabosenko@o-s-i.org>
2023-11-16 15:43:02 +0200

    homepage

--- views/page/home.php
+++ views/page/home.php
@@ -14,6 +14,18 @@
     </style>
 </head>
 <body>
+<h1 class="text-center text-lg-start mt-4 pt-2">
+    Hi, this is a sample of home page with iframe
+</h1>
+<div class="text-center text-lg-start mt-4 pt-2">
+    <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="<?php echo App\Route\RouteRegistry::getRegisteredRoute('register') ?>" class="link-danger">Register</a></p>
+</div>
+<div class="text-center text-lg-start mt-4 pt-2">
+    <p class="small fw-bold mt-2 pt-1 mb-0">Or just log in <a href="<?php echo App\Route\RouteRegistry::getRegisteredRoute('login') ?>" class="link-danger">Login</a></p>
+</div>
+<br>
+<br>
+<br>
 <iframe src="https://en.wikipedia.org/wiki/PHP" title="Embedded Website"></iframe>
 </body>
 </html>
